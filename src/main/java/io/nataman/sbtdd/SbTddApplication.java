c5180c2581bdb975744fe308ef5f8d9745a2b534
Manikandan Natarajan <manikann@gmail.com>
2020-05-26 22:55:19 +0800

    Code generated by spring initializer

---
+++ src/main/java/io/nataman/sbtdd/SbTddApplication.java
@@ -0,0 +1,12 @@
+package io.nataman.sbtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTddApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbTddApplication.class, args);
+  }
+}
---
+++ src/test/java/io/nataman/sbtdd/SbTddApplicationTests.java
@@ -0,0 +1,11 @@
+package io.nataman.sbtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTddApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
